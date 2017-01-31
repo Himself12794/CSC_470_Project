7d5813458c77c03f34f9a36c0ebe99c444374468
phwhitin <phwhitin@cisco.com>
2017-01-31 14:22:04 -0500

    backup

--- run.c
+++ run.c
@@ -1,7 +1,4 @@
-
 int main(int argc, char **argv) {
-	
-	system("java -jar ./target/reservation-system-0.0.1-SNAPSHOT.jar");
-	
+	system("java -jar ./target/reservation-system-0.0.1-SNAPSHOT.jar");	
 	return 0;
 }
